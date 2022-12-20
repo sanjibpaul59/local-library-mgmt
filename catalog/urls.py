718206061ebfbcb1694ce9cd77d332784a217877
sanjib3 <sanjib@shurjomukhi.com.bd>
2022-12-21 01:18:19 +0600

    Updated the Admin site and created views and added urls to see the views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = []
+# router = routers.DefaultRouter()
+# router.register(r"index", views.HomeView, basename="home")
+
+urlpatterns = [
+    path("", views.HomeView.as_view()),
+    # path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
